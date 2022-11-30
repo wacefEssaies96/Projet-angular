b06b2dd680a947f59f37c968eb6a9128edbbd1a9
wacefessaies1 <wacef.essaies@esprit.tn>
2022-11-30 18:55:26 +0000

    fixed minor bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DetailequipeModule } from './detailequipe/detailequipe.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
-import { DetailComponent } from './detailequipe/detail/detail.component'
 /*.In our AppModule, we then need to specify the component that will serve as the entry point component for our application.
 In our app.module.ts file where we import the entry component (conventionally AppComponent) and supply it as the only item
 in our bootstrap array inside the NgModule configuration object. eg. bootstrap[AppComponent]*/
@@ -24,8 +22,7 @@ in our bootstrap array inside the NgModule configuration object. eg. bootstrap[A
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule,
-    DetailequipeModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
